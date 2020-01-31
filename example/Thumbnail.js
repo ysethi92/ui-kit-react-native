20eb56c199c04e3b0a285ee7c2b28f0505d1dd8b
ved7111 <36672739+vedarshigawande@users.noreply.github.com>
2020-01-31 22:49:21 +0530

    example

---
+++ example/Thumbnail.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Thumbnail from './src/Thumbnail';
+
+export default class App extends React.Component {
+render() 
+{
+  const uri = "https://facebook.github.io/react-native/docs/assets/favicon.png";
+  return (
+    <View style={styles.container}>
+      <Text style={{fontWeight: 'bold'}}>Rounded Thumbnail</Text>
+      <Text>Small</Text> 
+      <Thumbnail Rounded size="small"  source={{uri: uri}} />
+      <Text>Medium</Text>
+      <Thumbnail Rounded size="medium"  source={{uri: uri}} />
+      <Text>Large</Text>
+      <Thumbnail Rounded size="large"  source={{uri: uri}} />
+      <Text style={{fontWeight:'bold'}}>Square Thumbnail</Text>
+      <Text> Small</Text>
+      <Thumbnail size="small" source={{uri: uri}} />
+      <Text>Medium</Text>
+      <Thumbnail size="medium" source={{uri: uri}} />
+      <Text>Large</Text>
+      <Thumbnail size="large" source={{uri: uri}} />
+    </View>
+  );
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
