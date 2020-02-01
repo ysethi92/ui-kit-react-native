241836e77208d6cc9be2d30d86e5a7ed86a11301
abhishek0518 <56636372+abhishek0518@users.noreply.github.com>
2020-02-01 10:14:55 +0530

    Update FB.js

--- example/FB.js
+++ example/FB.js
@@ -2,3 +2,8 @@
           onPress = {this.clickHandler}
           style =  {styles.FB} 
           imageUri = {require('./assets/plus-icon.png')}/>
+
+                      
+FB: {
+    backgroundColor : "#eb0028",
+  },
