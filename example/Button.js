4c27f58bbc726160ab96c71b1a2bcbe60c6de599
abhishek0518 <56636372+abhishek0518@users.noreply.github.com>
2020-02-01 09:36:55 +0530

    Create Button.js

---
+++ example/Button.js
@@ -0,0 +1,190 @@
+<View style={styles.containerText}>
+        <Text style={{ fontSize: 30,  }}>Button</Text>
+      </View>
+      <Button
+          text="Primary"
+          theme="primary"
+          onPress={() => null}
+      />
+      <Button
+          text="Success"
+          theme="success"
+          onPress={() => null}
+      />
+      <Button
+          text="Warning"
+          theme="warning"
+          onPress={() => null}
+      />
+      <Button
+          text="Danger"
+          theme="danger"
+          onPress={() => null}
+      />
+      <Button
+          text="Info"
+          theme="info"
+          onPress={() => null}
+      />
+
+      <View style={styles.containerText}>
+        <Text style={{ fontSize: 30,  }}>Round Button</Text>
+      </View>
+      <Button
+          text="Primary"
+          round
+          theme="primary"
+          onPress={() => null}
+      />
+      <Button
+          text="Success"
+          round
+          theme="success"
+          onPress={() => null}
+      />
+      <Button
+          text="Warning"
+          round
+          theme="warning"
+          onPress={() => null}
+      />
+      <Button
+          text="Danger"
+          round
+          theme="danger"
+          onPress={() => null}
+      />
+      <Button
+          text="Info"
+          round
+          theme="info"
+          onPress={() => null}
+      />
+
+      <View style={styles.containerText}>
+        <Text style={{ fontSize: 30,  }}>Outline Button</Text>
+      </View>
+      <Button
+          text="Primary"
+          round
+          outline
+          theme="primary"
+          onPress={() => null}
+      />
+      <Button
+          text="Success"
+          round
+          outline
+          theme="success"
+          onPress={() => null}
+      />
+      <Button
+          text="Warning"
+          round
+          outline
+          theme="warning"
+          onPress={() => null}
+      />
+      <Button
+          text="Danger"
+          round
+          outline
+          theme="danger"
+          onPress={() => null}
+      />
+      <Button
+          text="Info"
+          round
+          outline
+          theme="info"
+          onPress={() => null}
+      />
+
+      <View style={styles.containerText}>
+        <Text style={{ fontSize: 30,  }}>Full Button</Text>
+      </View>
+      <Button
+          full
+          text="Primary"
+          theme="primary"
+          onPress={() => null}
+      />
+      <Button
+          full
+          text="Success"
+          theme="success"
+          onPress={() => null}
+      />
+      <Button
+          full
+          text="Warning"
+          theme="warning"
+          onPress={() => null}
+      />
+      <Button
+          full
+          text="Danger"
+          theme="danger"
+          onPress={() => null}
+      />
+      <Button
+          full
+          text="Info"
+          theme="info"
+          onPress={() => null}
+      />
+
+      <View style={styles.containerText}>
+        <Text style={{ fontSize: 30,  }}>Disabled Button</Text>
+      </View>
+      <Button
+          disabled
+          text="Primary"
+          theme="primary"
+          onPress={() => null}
+      />
+      <Button
+          disabled
+          text="Success"
+          theme="success"
+          onPress={() => null}
+      />
+      <Button
+          disabled
+          text="Warning"
+          theme="warning"
+          onPress={() => null}
+      />
+      <Button
+          disabled
+          text="Danger"
+          theme="danger"
+          onPress={() => null}
+      />
+      <Button
+          disabled
+          text="Info"
+          theme="info"
+          onPress={() => null}
+      />
+
+      <View style={styles.containerText}>
+        <Text style={{ fontSize: 25,  }}>Different sizes</Text>
+      </View>
+      <Button
+          size = "small"
+          text="Small"
+          theme="info"
+          onPress={() => null}
+      />
+      <Button
+          text="Default"
+          theme="info"
+          onPress={() => null}
+      />
+      <Button
+          size = "large"
+          text="Large"
+          theme="info"
+          onPress={() => null}
+      />
