9d36fa69e1c630b2ca66523173089f356d9fc822
abhishek0518 <56636372+abhishek0518@users.noreply.github.com>
2020-02-01 09:42:06 +0530

    Update index.js

--- components/index.js
+++ components/index.js
@@ -2,3 +2,7 @@ export * from "./Button";
 export * from "./Thumbnail";
 export * from "./Header";
 export * from "./Avatar";
+export * from "./Spinner";
+export * from "./Tile";
+export * from "./Texte";
+export * from "./FB";
