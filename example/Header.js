bbb0c4b62cc9265974fe61ffaa2840b92e747181
abhishek0518 <56636372+abhishek0518@users.noreply.github.com>
2020-02-01 09:40:26 +0530

    Create Header.js

---
+++ example/Header.js
@@ -0,0 +1,9 @@
+<Header
+        onPress={() => ToastAndroid.show('Please implment a specific task!!!', ToastAndroid.SHORT)}
+        search
+        back
+        hidden={true}
+        headerText="Header"
+        textSize={22}
+        backgroundColor="skyblue"
+      />
