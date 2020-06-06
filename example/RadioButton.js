36edfebc4af4efb871bd5492f6303c25f0c52aba
ved7111 <36672739+vedarshigawande@users.noreply.github.com>
2020-06-06 20:55:49 +0530

    Create RadioButton.js

---
+++ example/RadioButton.js
@@ -0,0 +1,15 @@
+const radioButtonEntries = [
+  {
+      text: 'Yes',
+  },
+  {
+      text: 'No',
+  },
+  {
+      text: 'None'
+  },
+];
+<RadioButton
+   entries={radioButtonEntries}
+   backgroundColor="pink" 
+ />
